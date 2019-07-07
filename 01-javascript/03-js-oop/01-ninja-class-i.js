21def510c34271dc774e7e3e20e90f645c34c449
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-07-07 01:39:37 -0600

    completed ninja ii

--- 01-javascript/03-js-oop/01-ninja-class-i.js
+++ 01-javascript/03-js-oop/01-ninja-class-i.js
@@ -9,5 +9,26 @@ function ninjaMaker(name){
     this.showStatus = function(){
         console.log("Name: "+this.name+" Health: "+this.health+" Speed: "+speed+" Strength: "+strength);
     }
+    this.kick = function(ninja){
+      if(!ninja || !ninja.health){
+        throw new Error('Target must have health')
+      }
+        ninja.health -= 15;
+        console.log(ninja.name+' was kicked by '+this.name+" and lost 15 health");
+        strength += 1;
+        console.log(strength)
+    }
+    this.punch = function(ninja){
+        if(!ninja || !ninja.health){
+            throw new Error('Target must have health');
+        }
+        ninja.health -= 5;
+        console.log(ninja.health)
+        console.log(ninja.name+" was punched by "+this.name+" and lost 5 health");
+    }
+    this.drinkSake = function(){
+        self.health += 10;
+        console.log("ahh, refreshing!")
+    }
 
 }
