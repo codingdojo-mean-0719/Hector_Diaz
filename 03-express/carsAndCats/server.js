e36e99db0e4f7f44b037fa99d1754382fd2b569d
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-07-09 17:57:44 -0600

    finished ejs cars and cats. man I am into how stream line express is

--- 03-express/carsAndCats/server.js
+++ 03-express/carsAndCats/server.js
@@ -2,12 +2,21 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static(__dirname + "/static"))
+app.set('views',__dirname + "/views");
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/static"));
 
-app.get('/cars.html')
-app.get('/cats.html')
-app.get('/form.html')
+app.get('/cars', function(request,response){
+    response.render('cars');
+});
+
+app.get('/cats', function(request,response){
+    response.render('cats');
+});
+app.get('/cars/new', function(request,response){
+    response.render('form');
+});
 
 app.listen(8000, function(){
-    console.log("listening on port 8000")
-})
+    console.log("listening on port 8000");
+});
